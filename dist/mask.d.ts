be819c13019fc1cee8fbddcb3f7ad78836396fde
ion-project <gabrielcursino.medeiros@gmail.com>
2021-04-07 22:06:22 -0300

    chore(): Run build

--- dist/mask.d.ts
+++ dist/mask.d.ts
@@ -1,1 +1,5 @@
-export {};
+export declare function Mask(el: HTMLInputElement, options: any): any;
+export declare namespace Mask {
+    var masks: any;
+    var register: (name: string, mask: any) => void;
+}
