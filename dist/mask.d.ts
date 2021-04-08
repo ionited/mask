2cd5010d5f4960b54fadb593fc8a9d20e8f8c4e0
ion-project <gabrielcursino.medeiros@gmail.com>
2021-04-08 19:26:25 -0300

    chore(): Ignore dist/

--- dist/mask.d.ts
+++
@@ -1,5 +0,0 @@
-export declare function Mask(el: HTMLInputElement, options: any): any;
-export declare namespace Mask {
-    var masks: any;
-    var register: (name: string, mask: any) => void;
-}
--- dist/masks/default.d.ts
+++
@@ -1,9 +0,0 @@
-import { MaskData, MaskOptions } from '../core';
-export declare class MaskDefault implements MaskOptions {
-    firstInput: number;
-    mask: string;
-    maskGroups: any[];
-    constructor(el: HTMLInputElement, mask: string);
-    format(data: MaskData): void;
-    private getMaskGroups;
-}
