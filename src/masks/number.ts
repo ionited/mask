9b4593f171636117628c436717a3b4de2b21a8ed
ion-project <gabrielcursino.medeiros@gmail.com>
2021-04-23 21:56:46 -0300

    fix(): MaskNumber correct default value

--- src/masks/number.ts
+++ src/masks/number.ts
@@ -32,7 +32,7 @@ export class MaskNumber implements MaskOptions {
   }
 
   init(data: MaskData) {
-    data.input = data.input ? data.input : `0${this.getDecimal()}`;
+    data.input = data.input ? data.input.replace(/\./g, this.options.decimalPoint) : `0${this.getDecimal()}`;
 
     this.input(data);
   }
@@ -47,8 +47,8 @@ export class MaskNumber implements MaskOptions {
 
     data.input = input ? (
       decimal === -1 ? 
-        `${integer ? integer.substr(0, integer.length - (this.options.decimal ?? 0)) : '0'}${this.getDecimal(integer.substr(-(this.options.decimal ?? 0)))}` :
-        `${integer ? integer : '0'}${this.getDecimal(input.substr(decimal + 1, this.options.decimal))}`
+        `${integer ? integer : '0'}${this.getDecimal()}`
+        : `${integer ? integer : '0'}${this.getDecimal(input.substr(decimal + 1, this.options.decimal))}`
     ) : `0${this.getDecimal()}`;
   }
 
