c7f22f6f79d5d6c04b82687bd40f955609ff87a0
ion-project <gabrielcursino.medeiros@gmail.com>
2022-07-06 20:09:38 -0300

    style: format

--- src/mask.ts
+++ src/mask.ts
@@ -2,15 +2,13 @@ import { MaskDefault } from './masks/default';
 import { MaskNumber } from './masks/number';
 
 export function Mask(el: HTMLInputElement, options: any) {
-    for (let o in options) {
-        if (Mask.masks[o]) return new Mask.masks[o](el, options[o]);
-    }
+  for (let o in options) if (Mask.masks[o]) return new Mask.masks[o](el, options[o]);
 }
 
 Mask.masks = {} as any;
 
 Mask.register = function(name: string, mask: any) {
-    this.masks[name] = mask;
+  this.masks[name] = mask;
 }
 
 Mask.register('mask', MaskDefault);
